2cd35b7124567cd86ed7e05675ff546b6ae807f5
Menolo-Homobovanez <118155256+Menolo-Homobovanez@users.noreply.github.com>
2024-01-31 16:58:13 -0500

    copy pasted book class

--- src/MyTreeMap.java
+++ src/MyTreeMap.java
@@ -1,279 +1,234 @@
 import java.util.Collection;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Implementation of a Map using a binary search tree.
+ *
+ * @param <K>
+ * @param <V>
+ *
+ */
 public class MyTreeMap<K, V> implements Map<K, V> {
+
     private int size = 0;
     private Node root = null;
 
+    /**
+     * Represents a node in the tree.
+     *
+     */
     protected class Node {
         public K key;
         public V value;
         public Node left = null;
         public Node right = null;
+
+        /**
+         * @param key
+         * @param value
+         * @param left
+         * @param right
+         */
         public Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
     }
 
-
-    /**
-     * Returns the number of key-value mappings in this map.  If the
-     * map contains more than {@code Integer.MAX_VALUE} elements, returns
-     * {@code Integer.MAX_VALUE}.
-     *
-     * @return the number of key-value mappings in this map
-     */
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    /**
-     * Returns {@code true} if this map contains no key-value mappings.
-     *
-     * @return {@code true} if this map contains no key-value mappings
-     */
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    /**
-     * Returns {@code true} if this map contains a mapping for the specified
-     * key.  More formally, returns {@code true} if and only if
-     * this map contains a mapping for a key {@code k} such that
-     * {@code Objects.equals(key, k)}.  (There can be
-     * at most one such mapping.)
-     *
-     * @param key key whose presence in this map is to be tested
-     * @return {@code true} if this map contains a mapping for the specified
-     * key
-     * @throws ClassCastException   if the key is of an inappropriate type for
-     *                              this map
-     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException if the specified key is null and this map
-     *                              does not permit null keys
-     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     */
-    @Override
-    public boolean containsKey(Object key) {
-        return false;
-    }
-
-    /**
-     * Returns {@code true} if this map maps one or more keys to the
-     * specified value.  More formally, returns {@code true} if and only if
-     * this map contains at least one mapping to a value {@code v} such that
-     * {@code Objects.equals(value, v)}.  This operation
-     * will probably require time linear in the map size for most
-     * implementations of the {@code Map} interface.
-     *
-     * @param value value whose presence in this map is to be tested
-     * @return {@code true} if this map maps one or more keys to the
-     * specified value
-     * @throws ClassCastException   if the value is of an inappropriate type for
-     *                              this map
-     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException if the specified value is null and this
-     *                              map does not permit null values
-     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     */
-    @Override
-    public boolean containsValue(Object value) {
-        return false;
-    }
-
-    /**
-     * Returns the value to which the specified key is mapped,
-     * or {@code null} if this map contains no mapping for the key.
-     *
-     * <p>More formally, if this map contains a mapping from a key
-     * {@code k} to a value {@code v} such that
-     * {@code Objects.equals(key, k)},
-     * then this method returns {@code v}; otherwise
-     * it returns {@code null}.  (There can be at most one such mapping.)
-     *
-     * <p>If this map permits null values, then a return value of
-     * {@code null} does not <i>necessarily</i> indicate that the map
-     * contains no mapping for the key; it's also possible that the map
-     * explicitly maps the key to {@code null}.  The {@link #containsKey
-     * containsKey} operation may be used to distinguish these two cases.
-     *
-     * @param key the key whose associated value is to be returned
-     * @return the value to which the specified key is mapped, or
-     * {@code null} if this map contains no mapping for the key
-     * @throws ClassCastException   if the key is of an inappropriate type for
-     *                              this map
-     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException if the specified key is null and this map
-     *                              does not permit null keys
-     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     */
-    @Override
-    public V get(Object key) {
-        return null;
-    }
-
-    /**
-     * Associates the specified value with the specified key in this map
-     * (optional operation).  If the map previously contained a mapping for
-     * the key, the old value is replaced by the specified value.  (A map
-     * {@code m} is said to contain a mapping for a key {@code k} if and only
-     * if {@link #containsKey(Object) m.containsKey(k)} would return
-     * {@code true}.)
-     *
-     * @param key   key with which the specified value is to be associated
-     * @param value value to be associated with the specified key
-     * @return the previous value associated with {@code key}, or
-     * {@code null} if there was no mapping for {@code key}.
-     * (A {@code null} return can also indicate that the map
-     * previously associated {@code null} with {@code key},
-     * if the implementation supports {@code null} values.)
-     * @throws UnsupportedOperationException if the {@code put} operation
-     *                                       is not supported by this map
-     * @throws ClassCastException            if the class of the specified key or value
-     *                                       prevents it from being stored in this map
-     * @throws NullPointerException          if the specified key or value is null
-     *                                       and this map does not permit null keys or values
-     * @throws IllegalArgumentException      if some property of the specified key
-     *                                       or value prevents it from being stored in this map
-     */
-    @Override
-    public V put(K key, V value) {
-        return null;
-    }
-
-    /**
-     * Removes the mapping for a key from this map if it is present
-     * (optional operation).   More formally, if this map contains a mapping
-     * from key {@code k} to value {@code v} such that
-     * {@code Objects.equals(key, k)}, that mapping
-     * is removed.  (The map can contain at most one such mapping.)
-     *
-     * <p>Returns the value to which this map previously associated the key,
-     * or {@code null} if the map contained no mapping for the key.
-     *
-     * <p>If this map permits null values, then a return value of
-     * {@code null} does not <i>necessarily</i> indicate that the map
-     * contained no mapping for the key; it's also possible that the map
-     * explicitly mapped the key to {@code null}.
-     *
-     * <p>The map will not contain a mapping for the specified key once the
-     * call returns.
-     *
-     * @param key key whose mapping is to be removed from the map
-     * @return the previous value associated with {@code key}, or
-     * {@code null} if there was no mapping for {@code key}.
-     * @throws UnsupportedOperationException if the {@code remove} operation
-     *                                       is not supported by this map
-     * @throws ClassCastException            if the key is of an inappropriate type for
-     *                                       this map
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     * @throws NullPointerException          if the specified key is null and this
-     *                                       map does not permit null keys
-     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
-     */
-    @Override
-    public V remove(Object key) {
-        return null;
-    }
-
-    /**
-     * Copies all of the mappings from the specified map to this map
-     * (optional operation).  The effect of this call is equivalent to that
-     * of calling {@link #put(Object, Object) put(k, v)} on this map once
-     * for each mapping from key {@code k} to value {@code v} in the
-     * specified map.  The behavior of this operation is undefined if the
-     * specified map is modified while the operation is in progress.
-     *
-     * @param m mappings to be stored in this map
-     * @throws UnsupportedOperationException if the {@code putAll} operation
-     *                                       is not supported by this map
-     * @throws ClassCastException            if the class of a key or value in the
-     *                                       specified map prevents it from being stored in this map
-     * @throws NullPointerException          if the specified map is null, or if
-     *                                       this map does not permit null keys or values, and the
-     *                                       specified map contains null keys or values
-     * @throws IllegalArgumentException      if some property of a key or value in
-     *                                       the specified map prevents it from being stored in this map
-     */
-    @Override
-    public void putAll(Map<? extends K, ? extends V> m) {
-
-    }
-
-    /**
-     * Removes all of the mappings from this map (optional operation).
-     * The map will be empty after this call returns.
-     *
-     * @throws UnsupportedOperationException if the {@code clear} operation
-     *                                       is not supported by this map
-     */
     @Override
     public void clear() {
+        size = 0;
+        root = null;
+    }
 
+    @Override
+    public boolean containsKey(Object target) {
+        return findNode(target) != null;
     }
 
     /**
-     * Returns a {@link Set} view of the keys contained in this map.
-     * The set is backed by the map, so changes to the map are
-     * reflected in the set, and vice-versa.  If the map is modified
-     * while an iteration over the set is in progress (except through
-     * the iterator's own {@code remove} operation), the results of
-     * the iteration are undefined.  The set supports element removal,
-     * which removes the corresponding mapping from the map, via the
-     * {@code Iterator.remove}, {@code Set.remove},
-     * {@code removeAll}, {@code retainAll}, and {@code clear}
-     * operations.  It does not support the {@code add} or {@code addAll}
-     * operations.
+     * Returns the entry that contains the target key, or null if there is none.
      *
-     * @return a set view of the keys contained in this map
+     * @param target
      */
+    private Node findNode(Object target) {
+        // some implementations can handle null as a key, but not this one
+        if (target == null) {
+            throw new IllegalArgumentException();
+        }
+
+        // something to make the compiler happy
+        @SuppressWarnings("unchecked")
+        Comparable<? super K> k = (Comparable<? super K>) target;
+
+        // TODO: FILL THIS IN!
+        return null;
+    }
+
+    /**
+     * Compares two keys or two values, handling null correctly.
+     *
+     * @param target
+     * @param obj
+     * @return
+     */
+    private boolean equals(Object target, Object obj) {
+        if (target == null) {
+            return obj == null;
+        }
+        return target.equals(obj);
+    }
+
+    @Override
+    public boolean containsValue(Object target) {
+        return containsValueHelper(root, target);
+    }
+
+    private boolean containsValueHelper(Node node, Object target) {
+        // TODO: FILL THIS IN!
+        return false;
+    }
+
+    @Override
+    public Set<Map.Entry<K, V>> entrySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V get(Object key) {
+        Node node = findNode(key);
+        if (node == null) {
+            return null;
+        }
+        return node.value;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
     @Override
     public Set<K> keySet() {
+        Set<K> set = new LinkedHashSet<K>();
+        // TODO: FILL THIS IN!
+        return set;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        if (key == null) {
+            throw new NullPointerException();
+        }
+        if (root == null) {
+            root = new Node(key, value);
+            size++;
+            return null;
+        }
+        return putHelper(root, key, value);
+    }
+
+    private V putHelper(Node node, K key, V value) {
+        // TODO: FILL THIS IN!
         return null;
     }
 
-    /**
-     * Returns a {@link Collection} view of the values contained in this map.
-     * The collection is backed by the map, so changes to the map are
-     * reflected in the collection, and vice-versa.  If the map is
-     * modified while an iteration over the collection is in progress
-     * (except through the iterator's own {@code remove} operation),
-     * the results of the iteration are undefined.  The collection
-     * supports element removal, which removes the corresponding
-     * mapping from the map, via the {@code Iterator.remove},
-     * {@code Collection.remove}, {@code removeAll},
-     * {@code retainAll} and {@code clear} operations.  It does not
-     * support the {@code add} or {@code addAll} operations.
-     *
-     * @return a collection view of the values contained in this map
-     */
+    @Override
+    public void putAll(Map<? extends K, ? extends V> map) {
+        for (Map.Entry<? extends K, ? extends V> entry: map.entrySet()) {
+            put(entry.getKey(), entry.getValue());
+        }
+    }
+
+    @Override
+    public V remove(Object key) {
+        // OPTIONAL TODO: FILL THIS IN!
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
     @Override
     public Collection<V> values() {
-        return null;
+        Set<V> set = new HashSet<V>();
+        Deque<Node> stack = new LinkedList<Node>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            if (node == null) continue;
+            set.add(node.value);
+            stack.push(node.left);
+            stack.push(node.right);
+        }
+        return set;
     }
 
     /**
-     * Returns a {@link Set} view of the mappings contained in this map.
-     * The set is backed by the map, so changes to the map are
-     * reflected in the set, and vice-versa.  If the map is modified
-     * while an iteration over the set is in progress (except through
-     * the iterator's own {@code remove} operation, or through the
-     * {@code setValue} operation on a map entry returned by the
-     * iterator) the results of the iteration are undefined.  The set
-     * supports element removal, which removes the corresponding
-     * mapping from the map, via the {@code Iterator.remove},
-     * {@code Set.remove}, {@code removeAll}, {@code retainAll} and
-     * {@code clear} operations.  It does not support the
-     * {@code add} or {@code addAll} operations.
-     *
-     * @return a set view of the mappings contained in this map
+     * @param args
      */
-    @Override
-    public Set<Entry<K, V>> entrySet() {
-        return null;
+    public static void main(String[] args) {
+        Map<String, Integer> map = new MyTreeMap<String, Integer>();
+        map.put("Word1", 1);
+        map.put("Word2", 2);
+        Integer value = map.get("Word1");
+        System.out.println(value);
+
+        for (String key: map.keySet()) {
+            System.out.println(key + ", " + map.get(key));
+        }
+    }
+
+    /**
+     * Makes a node.
+     *
+     * This is only here for testing purposes.  Should not be used otherwise.
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public MyTreeMap<K, V>.Node makeNode(K key, V value) {
+        return new Node(key, value);
+    }
+
+    /**
+     * Sets the instance variables.
+     *
+     * This is only here for testing purposes.  Should not be used otherwise.
+     *
+     * @param node
+     * @param size
+     */
+    public void setTree(Node node, int size ) {
+        this.root = node;
+        this.size = size;
+    }
+
+    /**
+     * Returns the height of the tree.
+     *
+     * This is only here for testing purposes.  Should not be used otherwise.
+     *
+     * @return
+     */
+    public int height() {
+        return heightHelper(root);
+    }
+
+    private int heightHelper(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = heightHelper(node.left);
+        int right = heightHelper(node.right);
+        return Math.max(left, right) + 1;
     }
 }
